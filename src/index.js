7c98e34b1df7988b4bee4ec74ebc646b29ed7573
JanCesenek <jan.cesenekCZ@gmail.com>
2025-04-13 10:28:44 +0200

    node version fix?

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ const usersRoutes = require("../routes/users");
 const itemsRoutes = require("../routes/items");
 const messagesRoutes = require("../routes/messages");
 const reviewsRoutes = require("../routes/reviews");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = +process.env.PORT || 8080;
+const uuid = uuidv4();
 
 const corsOptions = {
   origin: "*",
@@ -17,6 +19,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.json(`Success! 😊 Today's lucky code is ${uuid}`);
+});
 
 app.use(authRoutes);
 app.use(usersRoutes);
